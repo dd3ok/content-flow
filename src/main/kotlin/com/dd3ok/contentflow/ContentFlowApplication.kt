1a0bb6bb92fe611e9d5e64afa57c1629f7954388
dd3ok <hwick@nate.com>
2025-07-09 17:34:14 +0900

    init

---
+++ src/main/kotlin/com/dd3ok/contentflow/ContentFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.dd3ok.contentflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContentFlowApplication
+
+fun main(args: Array<String>) {
+    runApplication<ContentFlowApplication>(*args)
+}
---
+++ src/test/kotlin/com/dd3ok/contentflow/ContentFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dd3ok.contentflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContentFlowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
